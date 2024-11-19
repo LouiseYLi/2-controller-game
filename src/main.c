f92ca1e8e89d7b5ec49f666b50d3abdcde18eb6c
LouiseYLi <louiseli.van@gmail.com>
2024-11-18 22:27:20 -0800

    replaced code in main with invoking gui()

--- src/main.c
+++ src/main.c
@@ -1,60 +1,12 @@
 #include "../include/display.h"
-#include "../include/input_processing.h"
+#include "../include/gui.h"
 #include <SDL2/SDL.h>
 #include <errno.h>
-#include <ncurses.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-    char c = ' ';
-    int  yCoord;
-    int  xCoord;
-    int  direction_y = 0;
-    int  direction_x = 0;
-
-    initscr();
-    noecho();
-    curs_set(0);
-
-    yCoord = (LINES - 1) / 2;
-    xCoord = (COLS - 1) / 2;
-
-    do
-    {
-        clear();
-        if(c == 'w')
-        {
-            direction_y = -1;
-            direction_x = 0;
-        }
-        if(c == 'a')
-        {
-            direction_y = 0;
-            direction_x = -1;
-        }
-        if(c == 's')
-        {
-            direction_y = 1;
-            direction_x = 0;
-        }
-        if(c == 'd')
-        {
-            direction_y = 0;
-            direction_x = 1;
-        }
-        if(hit_borders(COLS, LINES, xCoord, yCoord, direction_x, direction_y) == 1)
-        {
-            yCoord += direction_y;
-            xCoord += direction_x;
-        }
-        mvaddch(yCoord, xCoord, '*');
-        refresh();
-        c = (char)getch();
-    } while(c == 'w' || c == 'a' || c == 's' || c == 'd');
-
-    endwin();
-    return EXIT_SUCCESS;
+    gui();
 }
