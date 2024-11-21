3b3ddb7dc6591d8fcbbb6aa69cd41619a1507090
LouiseYLi <louiseli.van@gmail.com>
2024-11-20 18:52:17 -0800

    implemented parse_arguments(..) to parse command line arguments

--- src/main.c
+++ src/main.c
@@ -12,23 +12,63 @@
 // TODO: implement "other" peer
 // TODO: Implement sockets for peer
 
-int main(void)
+static int parse_arguments(int argc, char *argv[], void *arg)
+{
+    int                    option;
+    int                    retval;
+    struct network_socket *data = (struct network_socket *)arg;
+    data->ip                    = NULL;
+    while((option = getopt(argc, argv, "i:")) != -1)
+    {
+        if(option == 'i')
+        {
+            data->ip = optarg;
+        }
+        else
+        {
+            perror("Error: invalid options.");
+            retval = -1;
+            goto done;
+        }
+    }
+    if(data->ip == NULL)
+    {
+        perror("Error: unable to parse ip.");
+        retval = -1;
+        goto done;
+    }
+    retval = 0;
+done:
+    return retval;
+}
+
+int main(int argc, char *argv[])
 {
     struct network_socket data;
-    int err = 0;
-    int retval = 0;
-    
-    const char *PORT = "9999";
-    data.ip = NULL;
-    data.port = convert_port(PORT, &err);
-    data.socket_fd = 0;
+    int                   err    = 0;
+    int                   retval = 0;
 
-    gui(&err);
+    const char *PORT = "9999";
+    data.ip          = NULL;
+    data.port        = convert_port(PORT, &err);
+    data.socket_fd   = 0;
+
+    if(parse_arguments(argc, argv, &data) == -1)
+    {
+        perror("Error parsing arguments.");
+        retval = EXIT_FAILURE;
+        goto done;
+    }
+
+    printf("ip: %s", data.ip);
+
+    // gui(&err);
 
     // cleanup:
-    if (data.socket_fd != 0) {
+    if(data.socket_fd != 0)
+    {
         close_socket(data.socket_fd);
     }
-    // done:
+done:
     return retval;
 }
