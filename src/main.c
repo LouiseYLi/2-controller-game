bc26fd13cbd62b2a1f1df433b9bc139a25ac2507
LouiseYLi <louiseli.van@gmail.com>
2024-11-27 16:26:29 -0800

    refactored to accept cmd line arg for the input control type

--- src/main.c
+++ src/main.c
@@ -16,14 +16,29 @@
 // TODO: implement "other" peer
 // TODO: Implement sockets for peer
 
-static void parse_arguments(int argc, char *argv[], void *arg, int *err)
+static void parse_arguments(int argc, char *argv[], void *arg, game *g, int *err)
 {
+    const int              base = 10;
+    char                  *endptr;
     int                    option;
     struct network_socket *data = (struct network_socket *)arg;
-    while((option = getopt(argc, argv, "s:d:")) != -1)
+    while((option = getopt(argc, argv, "i:s:d:")) != -1)
     {
         switch(option)
         {
+            case 'i':
+                g->input_type = (int)strtol(optarg, &endptr, base);
+                if(endptr == optarg)
+                {
+                    // No conversion was performed
+                    perror("Error invalid input type. Input type must be 1 for keyboard, 2 for controller, 3 for timer.");
+                }
+                else if(*endptr != '\0')
+                {
+                    // Conversion stopped at a non-digit character
+                    perror("Error: Invalid characters found");
+                }
+                break;
             case 's':
                 data->src_ip = optarg;
                 break;
@@ -50,13 +65,14 @@ int main(int argc, char *argv[])
     //=========
     struct network_socket data;
     // Temp default values for window and players
-    const int height = 20;
-    const int width  = 30;
-    game      g      = {3, height, width, NULL};
-    player    p      = {0, temp_coord, temp_coord};
-    player    p2     = {0, temp_coord2, temp_coord2};
-    int       err    = 0;
-    int       retval = 0;
+    const int MAX_ARGS = 7;
+    const int height   = 25;
+    const int width    = 50;
+    game      g        = {0, height, width, NULL};
+    player    p        = {0, temp_coord, temp_coord};
+    player    p2       = {0, temp_coord2, temp_coord2};
+    int       err      = 0;
+    int       retval   = 0;
 
     const char *PORT = "1532";
 
@@ -65,11 +81,24 @@ int main(int argc, char *argv[])
     data.port      = convert_port(PORT, &err);
     data.socket_fd = 0;
 
+    if(MAX_ARGS != argc)
+    {
+        perror("Error invalid number of arguments");
+        return retval;
+    }
+
     // Set the desired sleep time in nanoseconds
     req.tv_sec  = 0;
     req.tv_nsec = sleep_time;    // 100 milliseconds
     if(err != 0)
     {
+        goto done;
+    }
+
+    parse_arguments(argc, argv, &data, &g, &err);
+    if(err != 0)
+    {
+        retval = EXIT_FAILURE;
         goto done;
     }
 
@@ -81,13 +110,6 @@ int main(int argc, char *argv[])
         {
             goto done;
         }
-    }
-
-    parse_arguments(argc, argv, &data, &err);
-    if(err != 0)
-    {
-        retval = EXIT_FAILURE;
-        goto done;
     }
 
     setup_host_socket(&data, &err);
